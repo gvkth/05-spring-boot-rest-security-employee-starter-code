86355589d5a6ce7a090c5fe99c4064faa04ac15a
cuongvk <cuongvk.hcm@vnpt.vn>
2023-06-09 23:15:24 +0700

    Học về Spring Security Thử nghiệm với user tạo trong db, password lưu dạng plain text, và khai thác bằng JDBC.

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,14 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+
+    //adđ support for JDBC ... no more hardcoded users
+    //tell Spring Security to use JDBC authentication with our data source
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
                 .username("john")
@@ -30,7 +40,7 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
